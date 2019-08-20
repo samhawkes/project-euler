2f94619d43234830881f773d2fe7198c8a556ae4
Sam Hawkes <40278032+samhawkes@users.noreply.github.com>
2019-08-20 09:35:57 +0100

    Added the fibonacci number helpers and tests

--- Solutions.Tests/Helpers/FibonacciNumbersTests.cs
+++ Solutions.Tests/Helpers/FibonacciNumbersTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Solutions.Helpers;
 using Xunit;
 
@@ -12,5 +13,38 @@ namespace Solutions.Tests.Helpers
             _fibonacciNumbers = new FibonacciNumbers();
         }
 
+        [Theory]
+        [InlineData(10)]
+        [InlineData(100)]
+        public void GetNFibonacciNumbers(int n)
+        {
+            var fibonacciNumbers = _fibonacciNumbers.GetNFibonacciNumbers(n);
+            
+            Assert.True(fibonacciNumbers.Count().Equals(n));
+        }
+
+        [Theory]
+        [InlineData(10, 55)]
+        [InlineData(30, 832040)]
+        public void GetTheNthFibonacciNumber(int n, ulong expectedValue)
+        {
+            Assert.Equal(_fibonacciNumbers.GetTheNthFibonacciNumber(n), expectedValue);
+        }
+
+        [Theory]
+        [InlineData(60, 55)]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(4, 3)]
+        [InlineData(832040, 832040)]
+        [InlineData(832041, 832040)]
+        [InlineData(832039, 514229)]
+        public void GetFibonacciNumbersUpToN(ulong n, ulong expectedLastNumber)
+        {
+            var fibonacciNumbers = _fibonacciNumbers.GetFibonacciNumbersUpToN(n);
+            
+            Assert.Equal(fibonacciNumbers.Last(), expectedLastNumber);
+        }
     }
 }
--- Solutions/Helpers/FibonacciNumbers.cs
+++ Solutions/Helpers/FibonacciNumbers.cs
@@ -7,9 +7,45 @@ namespace Solutions.Helpers
     {
         private readonly List<ulong> _fibonacciNumbers = new List<ulong> {1, 1};
 
+        public IEnumerable<ulong> GetFibonacciNumbersUpToN(ulong n)
+        {
+            if (_fibonacciNumbers.Last() >= n)
+                return _fibonacciNumbers.Where(x => x <= n);
+
+            while (_fibonacciNumbers.Last() < n)
+            {
+                AddNextFibonacciNumberToList();
+            }
+
+            if (_fibonacciNumbers.Last() > n)
+                _fibonacciNumbers.RemoveAt(_fibonacciNumbers.Count - 1);
+            
+            return _fibonacciNumbers;
+        }
+
+        public IEnumerable<ulong> GetNFibonacciNumbers(int n)
+        {
+            while (_fibonacciNumbers.Count < n)
+            {
+                AddNextFibonacciNumberToList();
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                yield return _fibonacciNumbers[i];
+            }
+        }
+        
+        public ulong GetTheNthFibonacciNumber(int n)
+        {
+            var fibonacciNumbers = GetNFibonacciNumbers(n);
+
+            return fibonacciNumbers.ElementAt(n - 1);
+        }
+        
         private void AddNextFibonacciNumberToList()
         {
-            var nextNumber = _fibonacciNumbers.Last() + _fibonacciNumbers.ElementAt(_fibonacciNumbers.Count - 1);
+            var nextNumber = _fibonacciNumbers.Last() + _fibonacciNumbers.ElementAt(_fibonacciNumbers.Count - 2);
             
             _fibonacciNumbers.Add(nextNumber);
         }
