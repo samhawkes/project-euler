7556d595c52cbc8b58b1156e8e6d5b67bd7e8d8c
Sam Hawkes <40278032+samhawkes@users.noreply.github.com>
2019-08-20 11:27:17 +0100

    Added problem 2 solution

---
+++ Solutions.Tests/Problems/Problem2Tests.cs
@@ -0,0 +1,21 @@
+using Xunit;
+
+namespace Solutions.Tests.Problems
+{
+    public class Problem2Tests
+    {
+        private IProblem _problem;
+
+        public Problem2Tests()
+        {
+            _problem = new Problem2();
+        }
+
+        [Theory]
+        [InlineData(4000000, 4613732)]
+        public void Problem1(int targetNumber, int expectedOutcome)
+        {
+            Assert.True(_problem.Run(targetNumber).Equals(expectedOutcome));
+        }
+    }
+}
--- Solutions/Problems/Problem2.cs
+++ Solutions/Problems/Problem2.cs
@@ -1,10 +1,21 @@
+using System;
+using System.Linq;
+using Solutions.Helpers;
+
 namespace Solutions
 {
     public class Problem2 : IProblem
     {
         public int Run(int? targetNumber)
         {
-            throw new System.NotImplementedException();
+            var fibonacci = new FibonacciNumbers();
+
+            if (targetNumber == null)
+                throw new ArgumentException("Please enter a target number.");
+            
+            var numbers = fibonacci.GetFibonacciNumbersUpToN((ulong) targetNumber);
+
+            return (int) numbers.Sum(num => num % 2d == 0d ? num : 0d);
         }
     }
 }
